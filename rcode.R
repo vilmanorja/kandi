26fed1aec4b92773b94ec160a047ee5cd284ec9a
Vilma Norja <vilma.norja@aalto.fi>
2022-06-13 09:31:58 +0300

    Small edits.

--- rcode.R
+++ rcode.R
@@ -1,17 +1,21 @@
 setwd("/m/home/home3/33/norjav2/unix/Documents/kandi_local/kandi")
 
+#read data
 data <- read.table(file = "anjala.csv", header = TRUE, sep = ",", col.names = 
                      c('Year', 'Month', 'Day', 'Hour', 'Time zone', 'Temperature'))
 
+#remove and edit data
 data_july <- data[data$Month == "7",]
 
-data_july2 <- data.frame(data_july$Year, data_july$Temperature)
+data_july2 <- data.frame(data_july$Year, data_july$Day, data_july$Temperature)
 
 temp_ts = ts(data_july$Temperature, start = 1959, frequency = 31)
 
 ts.plot(temp_ts, xlab = 'Year', ylab = 'top temperature/Celcius')
 
-mean_temp <- aggregate(data_july2$data_july.Temperature, list(data_july2$data_july.Year), FUN = mean)
-group <- aggregate(data_july2$data_july.Temperature, list(data_july2$data_july.Year))
+mean_temp <- aggregate(data_july$Temperature, list(data_july$Year), FUN = mean)
 
 plot(mean_temp)
+
+data$Date<-as.Date(with(data,paste(Year,Month,Day,sep="-")),"%Y-%m-%d")
+
